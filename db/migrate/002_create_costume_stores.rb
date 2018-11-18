97e0aa031c06f60b98ac16969affaad16ac0ab29
John Werenski <jtwerenski@gmail.com>
2018-11-18 18:56:46 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,6 @@
 # Create your costume_stores migration here
-class CreateCostumeStores< ActiveRecord::Migration
+class CreateCostumeStores< ActiveRecord::Migration 
+  def change
+    
+  end 
+end 
