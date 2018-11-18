258cd18f8d085b2cee4a2598b5f3affacf2a9fa4
John Werenski <jtwerenski@gmail.com>
2018-11-18 18:52:23 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord::Migration
       t.float :price 
       t.string :image_url 
       t.string :size 
-      t.timestamp
+      t.timestamps
     end 
   end 
 end 
